c8c723e6cd15263752b925ff3418f91cab944f7b
Muhammed Emin Yildirim <yldrmmuhammedemin@gmail.com>
2023-04-08 21:44:51 +0300

    Initial Commit

---
+++ GenericNetworkLayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenericNetworkLayer
+//
+//  Created by Yildirim on 8.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GenericNetworkLayerUITests/GenericNetworkLayerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GenericNetworkLayerUITestsLaunchTests.swift
+//  GenericNetworkLayerUITests
+//
+//  Created by Yildirim on 8.04.2023.
+//
+
+import XCTest
+
+final class GenericNetworkLayerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
